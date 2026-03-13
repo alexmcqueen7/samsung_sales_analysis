be2b19fa001b6d612bcb31557979ce8bda1c8c80
Alex McQueen <alex.mcqueen@hotmail.co.uk>
2026-03-13 14:09:20 +0000

    Add files via upload

---
+++ SQL/Performance by Region.sql
@@ -0,0 +1,9 @@
+SELECT
+    region,
+    EXTRACT(YEAR FROM sale_date) AS year,
+    ROUND(SUM(revenue), 2) AS total_revenue,
+    ROUND(SUM(units_sold), 2) AS total_units_sold,
+    COUNT(sale_id) AS total_orders
+FROM samsung_sales.sales_clean
+GROUP BY region, year
+ORDER BY region, year;
